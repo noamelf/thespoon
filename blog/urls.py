6ae15ad526f7c47b9f5d1edc9a582ebd29612806
noame <noam.elfenbaum@adk2.com>
2015-02-21 21:50:12 +0200

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from . import views, feed
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.BlogIndex.as_view(), name="index"),
+    url(r'^feed/$', feed.LatestPosts(), name="feed"),
+    url(r'^entry/(?P<slug>\S+)$', views.BlogDetail.as_view(), name="entry_detail"),
+)
+
+urlpatterns += patterns('django.contrib.flatpages.views',
+    url(r'^about-me/$', 'flatpage', {'url': '/about-me/'}, name='about-me'),
+)
