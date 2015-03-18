91b57d2be77953e231ad364c825ba561010ea271
noame <noam.elfenbaum@adk2.com>
2015-03-19 01:35:57 +0200

    blog ready

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,12 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from . import views, feed
+from django.views.generic import TemplateView
 
 
 urlpatterns = patterns(
     '',
     url(r'^$', views.BlogIndex.as_view(), name="index"),
+    url('^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^feed/$', feed.LatestPosts(), name="feed"),
     url(r'^entry/(?P<slug>\S+)$', views.BlogDetail.as_view(), name="entry_detail"),
-)
-
-urlpatterns += patterns('django.contrib.flatpages.views',
-    url(r'^about-me/$', 'flatpage', {'url': '/about-me/'}, name='about-me'),
 )
--- the_spoon/settings.py
+++ the_spoon/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'django.contrib.flatpages',
     'blog',
     'django_markdown'
 )
