2ae8929ec210cc7d1a292fae2eb36fdec634ac48
noame <noam.elfenbaum@adk2.com>
2015-03-27 15:47:27 +0300

    fixed psql static files

--- the_spoon/settings.py
+++ the_spoon/settings.py
@@ -1,33 +1,16 @@
-"""
-Django settings for the_spoon project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 SITE_ID = 1
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '2ybb65l!x_d4kxc6ewl9%9ocd33vom5_+1ox816_*(fju4uhk1'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 TEMPLATE_DEBUG = True
-
-# Application definition
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -55,33 +38,7 @@ ROOT_URLCONF = 'the_spoon.urls'
 
 WSGI_APPLICATION = 'the_spoon.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-#postgres://tmlwteehaoxbmk:Y8KAtCSqK0TBUVJmYHYnBQdiHS@ec2-23-21-231-14.compute-1.amazonaws.com:5432/d8f2iehd3ronjf
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'd8f2iehd3ronjf',
-#         'USER': 'tmlwteehaoxbmk',
-#         'PASSWORD': 'Y8KAtCSqK0TBUVJmYHYnBQdiHS',
-#         'HOST': 'ec2-23-21-231-14.compute-1.amazonaws.com', # Or something like this
-#         'PORT': '5432',
-#     }
-# }
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-import dj_database_url
 DATABASES = dict(default=dj_database_url.config())
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -93,31 +50,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
-
-
-# Parse database configuration from $DATABASE_URL
-# import dj_database_url
-
-# DATABASES['default'] = dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-# Static asset configuration
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../blog/static'),
 )
